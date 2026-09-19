649ac98d1cdebca0d9b7d92acab16c9639ecd755
Yousef Omar Hejazi <yohejazi@gmail.com>
2026-09-19 13:56:10 +0300

    f

--- sawaryfrontendPro/app/(public)/[lang]/works/design/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-import { localizedHref, type Locale } from '@/lib/i18n'
-
-export default async function WorksDesignPage({ params }: { params: Promise<{ lang: Locale }> }) {
-  const { lang } = await params
-  redirect(`${localizedHref(lang, '/works')}#design-projects`)
-}
